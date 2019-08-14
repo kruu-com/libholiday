2e4baf2f5396c8fe843a702a810007a782849d67
florianfreiburg <ff@schreiber-freunde.de>
2019-08-14 14:19:11 +0200

    Add state DK-85 (Sjaelland)

---
+++ src/Holiday/Dk/Dk85.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Holiday\Dk;
+
+class Dk85 extends Dk
+{
+    protected function getHolidays($year)
+    {
+        $data   = parent::getHolidays($year);
+
+        return $data;
+    }
+}
